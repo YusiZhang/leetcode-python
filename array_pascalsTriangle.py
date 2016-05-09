8425bb99c481d9c3f41e3f64dc6f083da0262bc9
Yusi Zhang <yusi.zhang@oracle.com>
2016-05-09 09:36:43 -0700

    add doc

--- array_pascalsTriangle.py
+++ array_pascalsTriangle.py
@@ -1,4 +1,20 @@
 class Solution(object):
+    """
+    Given numRows, generate the first numRows of Pascal's triangle.
+    For example, given numRows = 5,
+    Return
+
+    [
+         [1],
+        [1,1],
+       [1,2,1],
+      [1,3,3,1],
+     [1,4,6,4,1]
+    ]
+    Show Company Tags
+    Show Tags
+    Show Similar Problems
+    """
     def generate(self, numRows):
         """
         :type numRows: int
