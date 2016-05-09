70e6e4f62018f550e37b6e3367471a68652a45fd
Yusi Zhang <yusi.zhang@oracle.com>
2016-05-09 09:29:39 -0700

    optimize pascal triangle problem

--- array_pascalsTriangle.py
+++ array_pascalsTriangle.py
@@ -4,18 +4,21 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
+        if numRows == 0:
+            return []
         result = []
-        prev = []
-        for i in range(0,numRows):
-            current = []
-            for j in xrange(0,i):
-                if j == 0 or j == i:
-                    current.append(1)
-                else:
-                    current.append(prev[j-1] + prev[j])
-            prev = current
+        result.append([1])
+
+        for i in range(1, numRows):
+            current = [None] * (i + 1)
+            prev = result[i-1]
+            current[0] = prev[0]
+            current[i] = prev[i-1]
+            for j in range(1, i):
+                current[j] = prev[j-1] + prev[j]
             result.append(current)
         return result
+
 
 if __name__ == '__main__':
     print Solution().generate(5)
