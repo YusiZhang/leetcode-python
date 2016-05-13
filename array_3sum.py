9e926eff2fe2e713543a1fabc1936701a529e696
Yusi Zhang <yusi.zhang@oracle.com>
2016-05-13 09:54:06 -0700

    pascals triangle (unfinished)

--- array_3sum.py
+++ array_3sum.py
@@ -1,5 +1,6 @@
 __author__ = 'yusizhang'
 
+
 class Solution(object):
     def threeSum(self, nums):
         """
@@ -9,19 +10,19 @@ class Solution(object):
         res = []
         length = len(nums)
         nums.sort()
-        for i in range(0, length-2):
-            if i and nums[i] == nums[i-1]:
+        for i in range(0, length - 2):
+            if i and nums[i] == nums[i - 1]:
                 continue
             target = nums[i] * -1
-            left, right = i+1, length - 1
+            left, right = i + 1, length - 1
             while left < right:
                 if nums[left] + nums[right] == target:
                     res.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
-                    while left < right and nums[left-1] == nums[left]:
+                    while left < right and nums[left - 1] == nums[left]:
                         left += 1
-                    while left < right and nums[right+1] == nums[right]:
+                    while left < right and nums[right + 1] == nums[right]:
                         right -= 1
                 elif nums[left] + nums[right] > target:
                     right -= 1
