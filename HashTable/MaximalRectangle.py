d19bed67b8b5ea0f5ee999b9dc2d3c7669712764
Yusi Zhang <yusi.zhang@oracle.com>
2016-06-18 11:50:40 -0700

    06/18

--- HashTable/MaximalRectangle.py
+++ HashTable/MaximalRectangle.py
@@ -1,5 +1,6 @@
 """
-Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing all ones and return its area.
+Given a 2D binary matrix filled with 0's and 1's, 
+find the largest rectangle containing all ones and return its area.
 """
 class Solution(object):
     def maximalRectangle(self, matrix):
