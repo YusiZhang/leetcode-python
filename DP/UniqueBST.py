81e3862ccba41972869b31cae1c093fadb09a821
Yusi Zhang <yusi.zhang@oracle.com>
2016-07-29 09:43:22 -0700

    07/29

---
+++ DP/UniqueBST.py
@@ -0,0 +1,34 @@
+"""
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+"""
+
+"""
+Given a sequence 1…n, we pick a number i out of the sequence as the root, then the number of unique BST with the specified root F(i), is the cartesian product of the number of BST for its left and right subtrees. For example, F(3, 7): the number of unique BST tree with number 3 as its root. To construct an unique BST out of the entire sequence [1, 2, 3, 4, 5, 6, 7] with 3 as the root, which is to say, we need to construct an unique BST out of its left subsequence [1, 2] and another BST out of the right subsequence [4, 5, 6, 7], and then combine them together (i.e. cartesian product). The tricky part is that we could consider the number of unique BST out of sequence [1,2] as G(2), and the number of of unique BST out of sequence [4, 5, 6, 7] as G(4). Therefore, F(3,7) = G(2) * G(4).
+
+"""
+class Solution(object):
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        G = [0] * (n+1)
+        G[0] = G[1] = 1
+        for i in xrange(2, n+1):
+            for j in xrange(1, i+1):
+                G[i] += G[j-1] * G[i-j]
+        
+        return G[n]
+
+
+
+        
